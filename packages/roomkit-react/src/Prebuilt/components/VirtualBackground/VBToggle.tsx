75941015cb0ad75eb700c001b062255f7e3f3dd5
Alden Quimby <alden@civicroundtable.com>
2025-04-03 11:05:00 -0400

    feat: rt-2497 persist virtual background across meetings

--- packages/roomkit-react/src/Prebuilt/components/VirtualBackground/VBToggle.tsx
+++ packages/roomkit-react/src/Prebuilt/components/VirtualBackground/VBToggle.tsx
@@ -32,6 +32,7 @@ export const VBToggle = () => {
   useEffect(() => {
     if (!toggleVideo) {
       VBHandler?.reset();
+      // TODO-AQ do we need to change localStorage?
       hmsActions.setAppData(APP_DATA.background, HMSVirtualBackgroundTypes.NONE);
     }
   }, [hmsActions, toggleVideo]);
